08782a9b61ca9ecda944ece2a11c0ead11de14a4
rdheadyii <rdheadyii@gmail.com>
2023-07-30 14:45:39 -0400

    added pseudo code of what needs to be done on project

---
+++ server.js
@@ -0,0 +1,27 @@
+// import express/ fs/ path
+
+// define express as a variable
+
+// define the port
+
+// logic for generating id number
+
+// array for notes
+
+// need to set express variable for json/ html encoded
+
+// need static path for homepage
+
+// need a get
+
+// need a post
+
+// need a fs read file
+
+// need a fs write to file
+
+// need a delete
+
+// need a listener for port
+
+// will spread out between other JS pages (modularization)
